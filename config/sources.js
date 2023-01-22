28544f0ff1996b56bbba4583fc2a7127f9a20be4
codercove <codercove@gmail.com>
2023-01-22 19:48:08 +0530

    initialize repo

---
+++ config/sources.js
@@ -0,0 +1,18 @@
+export const sources = {
+  pastpaperwiki: {
+    10: {
+      main: "https://pastpapers.wiki/grade-10-ict/",
+      filters: [
+        "https://pastpapers.wiki/grade-10-information-",
+        "https://pastpapers.wiki/grade-10-ict-",
+      ],
+    },
+    11: {
+      main: "https://pastpapers.wiki/grade-11-ict/",
+      filters: [
+        "https://pastpapers.wiki/grade-11-ict-",
+        "https://pastpapers.wiki/grade-11-information-",
+      ],
+    },
+  },
+};
---
+++ index.js
@@ -0,0 +1,48 @@
+import Axios from "axios";
+import { load } from "cheerio";
+import Express from "express";
+import { sources } from "./config/sources.js";
+import { config } from "dotenv";
+config();
+const app = Express();
+const PORT = process.env.PORT || 8000;
+const filteredSources = [[], []];
+
+app.listen(PORT || 8000, console.log(`Server is running on PORT ${PORT}`));
+
+const start = (grade, n) => {
+  Axios(sources.pastpaperwiki[grade].main).then((res) => {
+    const html = res.data;
+    const $ = load(html);
+    $("a").each(function () {
+      let unFilteredElement = $(this).attr("href");
+      let unFilteredElementText = $(this).text();
+      for (let i in sources.pastpaperwiki[grade].filters) {
+        if (
+          unFilteredElement.startsWith(sources.pastpaperwiki[grade].filters[i])
+        ) {
+          Axios(unFilteredElement).then((resp) => {
+            const elm = resp.data;
+            const $$ = load(elm);
+            $$(".wpfd_downloadlink").each(function () {
+              let unFilteredElementURL = $$(this).attr("href");
+              filteredSources[n].push({
+                name: unFilteredElementText,
+                url: unFilteredElementURL,
+              });
+            });
+          });
+        }
+      }
+    });
+  });
+};
+
+start(10, 0);
+start(11, 1);
+setTimeout(() => {
+  console.log(filteredSources);
+  app.get("/sources", (req, res) => {
+    res.send(filteredSources);
+  });
+}, 2000);
