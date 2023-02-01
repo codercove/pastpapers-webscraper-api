3c6388a208261d7c21914eb56614b2f37fe0b5a0
Sithika Dangampala <105375428+codercove@users.noreply.github.com>
2023-02-01 18:39:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
 import Axios from "axios";
 import { load } from "cheerio";
 import Express from "express";
-import { sources } from "./config/sources.js";
 import { config } from "dotenv";
 
 config();
 const app = Express();
 const PORT = process.env.PORT || 8000;
-const filteredSources = [[], []];
+let filteredSources = [];
 
 
 app.listen(PORT || 8000, console.log(`Server is running on PORT ${PORT}`));
 
-const start = (grade, n) => {
-  Axios(sources.pastpaperwiki[grade].main).then((res) => {
+const start = (grade, sub) => {
+  filteredSources = []
+  Axios(`https://pastpapers.wiki/grade-${grade}-${sub}/`).then((res) => {
     const html = res.data;
     const $ = load(html);
     $("a").each(function () {
       let unFilteredElement = $(this).attr("href");
       let unFilteredElementText = $(this).text();
-      for (let i in sources.pastpaperwiki[grade].filters) {
-        if (
-          unFilteredElement.startsWith(sources.pastpaperwiki[grade].filters[i])
-        ) {
-          Axios(unFilteredElement).then((resp) => {
-            const elm = resp.data;
-            const $$ = load(elm);
-            $$(".wpfd_downloadlink").each(function () {
-              let unFilteredElementURL = $$(this).attr("href");
-              filteredSources[n].push({
-                name: unFilteredElementText,
-                url: unFilteredElementURL,
+        const arr = unFilteredElement.split('-')
+        for(let i in arr){
+          if(arr[i]=="paper"){
+            Axios(unFilteredElement).then((resp) => {
+              const elm = resp.data;
+              const $$ = load(elm);
+              $$(".wpfd_downloadlink").each(function () {
+                let unFilteredElementURL = $$(this).attr("href");
+                filteredSources.push({
+                  name: unFilteredElementText,
+                  url: unFilteredElementURL,
+                });
               });
             });
-          });
-        }
-      }
+          }
+        } 
     });
   });
 };
 
-start(10, 0);
-start(11, 1);
-setTimeout(() => {
-  app.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.send(filteredSources);
+
+  app.get("/:grade/:sub", (req, res) => {
+    start(req.params.grade,req.params.sub);
+    setTimeout(() => {
+    res
+      .setHeader("Access-Control-Allow-Origin", "*")
+      .status(201)
+      .send(filteredSources);
+  }, 2000);
+
   });
-}, 2000);
