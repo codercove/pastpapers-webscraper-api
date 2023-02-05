15a520388e003a649d0aa9eb762cfb7284b57ac7
Sithika Dangampala <105375428+codercove@users.noreply.github.com>
2023-02-05 23:51:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,58 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
-import dotenv from 'dotenv'
-import express from 'express'
+import axios from "axios";
+import cheerio from "cheerio";
+import dotenv from "dotenv";
+import express from "express";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 8000
+const app = express();
+const PORT = process.env.PORT || 8000;
 
- async function getSources (grade, subject) {
-  const filteredSources = []
-  const res = await axios(`https://pastpapers.wiki/grade-${grade}-${subject}/`)
-  const html = await res.data
-  const $ = cheerio.load(html)
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
 
-  //Filter All Anchor Elements
-  let j = 0;
-  $("a").each(async function(){
-    const unfilteredElement = $(this).attr('href')
-    const unFilteredElementText = $(this).text()
-    const splittedArr = unfilteredElement.split('-')
+app.get("/:grade/:sub", async (req, res) => {
+  const grade = req.params.grade;
+  const sub = req.params.sub;
+  const filteredSources = await getSources(grade, sub);
 
-    for (const i in splittedArr) {
-      if (splittedArr[i] === "paper") {
-        const resp = await axios(unfilteredElement)
-        const elm = await resp.data
-        const $$ = cheerio.load(elm)
+  if (filteredSources) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+      .status(200)
+      .send(filteredSources);
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+      .status(404)
+      .send("The page you requested not found");
+  }
+});
 
-        //filter elemts that have specific class
+async function getSources(grade, sub) {
+  const filteredSources = [];
+  const res = await axios(`https://pastpapers.wiki/grade-${grade}-${sub}/`);
+  const html = res.data;
+  const $ = cheerio.load(html);
 
-        $$(".wpfd_downloadlink").each(function() {
-          const unFilteredElementURL = $$(this).attr('href')
+  $("a").each(async function () {
+    const unFilteredElement = $(this).attr("href");
+    const unFilteredElementText = $(this).text();
+    const arr = unFilteredElement.split("-");
+    for (const i in arr) {
+      if (arr[i] === "paper") {
+        const resp = await axios(unFilteredElement);
+        const elm = resp.data;
+        const $$ = cheerio.load(elm);
+        $$(".wpfd_downloadlink").each(function () {
+          const unFilteredElementURL = $$(this).attr("href");
           filteredSources.push({
-
-            //paper id
-            id: j,
-
-            //paper name
             name: unFilteredElementText,
-
-            //paper donwload link
-            url: unFilteredElementURL
-
-          })
-          j += 1
-        })
+            url: unFilteredElementURL,
+          });
+        });
       }
     }
-  })
-  return filteredSources
+  });
+
+  return filteredSources;
 }
-
-//Listening to PORT
-
-app.listen(PORT, () => console.log(`App is listening to PORT : ${PORT}`))
-
-//Sending responses
-
-app.get('/', (req, res) => {
-  res
-    .status(201)
-    .send({
-      status: 201,
-      error: false,
-      message: 'API is Working successfully'
-    })
-})
-
-app.get('/:grade/:subject', async (req, res) => {
-  const grade = req.params.grade
-  const subject = req.params.subject
-
-  //Fetch download links of papers 
-  const filteredSources = await getSources(grade, subject)
-
-  const responses =  [{
-    //Successfull response
-    status: 200,
-    error: false,
-    message: 'no error - data fetched successfully',
-    items: filteredSources
-  },
-  {
-    //Unsuccessful response
-    status: 404,
-    error: true,
-    message: 'error - requested data not found',
-  }]
-
-  //Check if fetched data empty or not
-  if (responses[0].items) {
-    res
-      .setHeader('Access-Control-Allow-Origin', '*')
-      .status(201)
-      .send(responses[0])
-  } else {
-    res
-      .setHeader('Access-Control-Allow-Origin', '*')
-      .status(404)
-      .send(responses[1])
-  }
-
-})
